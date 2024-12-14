307f7ae6681704a53ed2a52126ef27506e7da860
Programmer-Jaf <programmerjaf@gmail.com>
2024-12-14 20:13:44 +0600

    learn about variables

---
+++ Java/Lecture 01 Basics/src/variables.java
@@ -0,0 +1,16 @@
+public class variables {
+	public static void main(String[] args) {
+		System.out.println("all about variables");
+//	* What is variable ?
+//	? ans : variables are container to store a value. For Example, a milk bottle 🍼 contains/hold milks.So, We can definitely tell that variables are containers.
+
+//	* datatype variableName = assignValue;
+		int value = 124;
+		System.out.println("int value is "+value);
+//	! Rules while creating variables
+//	* variable name can't start with number or any other special characters.
+//	* variable name start with letter,dollar sign ($),and underscore (_).
+//	* variable name can't be a keyword.
+//	* variable name must be meaningful.
+	}
+}
