a2744aea482535075c856d7d1994c9939b8b14c6
Programmer-Jaf <programmerjaf@gmail.com>
2024-12-14 21:04:49 +0600

    solve basic problem 03

---
+++ Java/Lecture 01 Basics/Basic Problems/src/basicProblem03.java
@@ -0,0 +1,8 @@
+public class basicProblem03 {
+	public static void main(String[] args) {
+		System.out.println("solve Write a program to print numbers from 1 to 100");
+		for (int i = 1; i <= 100; i++) {
+      System.out.print(i);
+    }
+	}
+}
