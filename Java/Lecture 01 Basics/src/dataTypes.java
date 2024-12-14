8b894bd897007bf0e53d36c278ba02d42d4b85c1
Programmer-Jaf <programmerjaf@gmail.com>
2024-12-14 20:28:35 +0600

    all about data types

---
+++ Java/Lecture 01 Basics/src/dataTypes.java
@@ -0,0 +1,51 @@
+public class dataTypes {
+	public static void main(String[] args) {
+//	* There are 2 types of datatype in java
+//	* primitive
+//	  * byte
+//	  * short
+//	  * int
+//	  * float
+//	  * double
+//	  * boolean
+//	  * char
+//	  * long
+//	* non-primitive
+//	  * array
+//	  * class
+//	  * Strings
+		System.out.println("Learn about Data types ");
+//	byte
+		byte byteValue = 127;
+		System.out.println("the byte value is "+byteValue);
+//	short
+		short shortValue = 32000;
+		System.out.println("the short value is "+shortValue);
+//  int
+		int intValue = 3254110;
+		System.out.println("the int value is "+intValue);
+//	float
+		float floatingValue = 325401.4415f;
+		System.out.println("the float value is "+floatingValue);
+//	double
+		double doubleValue = 325401.441554112;
+		System.out.println("the double value is "+doubleValue);
+//	long
+		long longestValue = 12464254126746496L;
+		System.out.println("the longest value is "+longestValue);
+//	boolean
+		boolean isAuthenticated = true;
+		System.out.println("the boolean value is "+isAuthenticated);
+//	char
+		char singleChar = 'a';
+		System.out.println("the single char is "+singleChar);
+
+//  Arrays
+		int[] arr = {1,2,3,4,5,6};
+		System.out.println("2nd index value in the array is "+arr[2]);
+//  Strings
+		String str = "my name is programmer-jaf";
+		System.out.println(str);
+//	! you can find classes codes in the OOPS Modules.Make Sure to check it out
+	}
+}
