7cd0328618b2c205dfaaaea66d197c445b372775
Programmer-Jaf <programmerjaf@gmail.com>
2024-12-14 19:59:28 +0600

    break down the java program

---
+++ Java/Lecture 01 Basics/src/breakProgram.java
@@ -0,0 +1,16 @@
+public class breakProgram {
+	public static void main(String[] args) {
+		/*
+			* public --> public is an access-modifier.
+			* class --> class is a user defined blue-print with some attributes and behaviours.
+			* breakProgram --> name of the public class. Must be matched with the file name
+			* static --> static allows to call methods without creating classes
+			* void --> void is the return type.void means null.It returns nothing
+			* main --> when a java program or c++ program start it start with main method
+			* (String[] args) --> It's a command line argument.
+			* System.out.println("") --> it means when main method called it will display as an output for users.
+			* ; --> make sure add semiColon add the end
+		 */
+		System.out.println("java program 🏗️ break throw");
+	}
+}
