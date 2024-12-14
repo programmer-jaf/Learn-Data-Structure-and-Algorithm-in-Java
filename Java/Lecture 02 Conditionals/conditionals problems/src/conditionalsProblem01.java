accdbf9789499b45e8cde6a38fcc1a68e7743cfd
Programmer-Jaf <programmerjaf@gmail.com>
2024-12-14 21:37:30 +0600

    solve conditionals problems 01

---
+++ Java/Lecture 02 Conditionals/conditionals problems/src/conditionalsProblem01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class conditionalsProblem01 {
+	public static void main(String[] args) {
+		System.out.println("Check Even or Odd 🔢");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number : ");
+		int num = sc.nextInt();
+		if (num%2==0) System.out.println(num+" is a even number");
+		else System.out.println(num+" is an odd number");
+	}
+}
